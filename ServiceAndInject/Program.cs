2ed784d595d03f78e1a567e9d952bb0366c91fd4
baljinder <baljindermaan15@gmail.com>
2024-10-05 15:03:18 -0400

    Service class added, Injected in program file

--- ServiceAndInject/Program.cs
+++ ServiceAndInject/Program.cs
@@ -1,6 +1,11 @@
+using ServiceAndInject.Interfaces;
+using ServiceAndInject.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddTransient<ICalculatorService<int>, CalculatorService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ ServiceAndInject/Services/CalculatorService.cs
@@ -0,0 +1,26 @@
+using ServiceAndInject.Interfaces;
+
+namespace ServiceAndInject.Services;
+
+public class CalculatorService : ICalculatorService<int>
+{
+    public int Add(int left, int right)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Divide(int left, int right)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Multiply(int left, int right)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int Subtract(int left, int right)
+    {
+        throw new NotImplementedException();
+    }
+}
